34fefaeb9ec40e65d63cc6a6c130e380c59ecbc4
AlexandrPro <Aleksandr_Prokofev_A@mail.ru>
2015-11-29 15:25:54 +0200

    Add one unit test

--- Solution/src/sample/Test/KeygenTest.java
+++ Solution/src/sample/Test/KeygenTest.java
@@ -1,9 +1,14 @@
 package sample.Test;
 
-
+import sample.Keygen;
 import org.junit.Test;
 import static junit.framework.Assert.*;
 
 
 public class KeygenTest {
+    @Test
+    public void test_gsd_5and7res1() throws Exception {
+        int t = Keygen.gcd(5, 7);
+        assertEquals(1, t);
+    }
 }
